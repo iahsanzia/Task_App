440088427930adbb331819e027aa1ac6b268a57a
Ahsan Zia <mynameisahsanzia@gmail.com>
2026-08-26 01:57:01 +0500

    prestart script added

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "node:path";
 import taskRoutes from "./routes/taskRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import cors from "cors";
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/tasks", taskRoutes);
+
+app.use(express.static(path.resolve(process.cwd(), "front-end")));
 
 app.use(errorHandler);
 
